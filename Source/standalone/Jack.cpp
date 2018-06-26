2436dd53ef0ebe1544f218467ea704df20a51112
JP Cimalando <jpcima@users.noreply.github.com>
2018-06-26 21:35:09 +0200

    handle signals

--- Source/standalone/Jack.cpp
+++ Source/standalone/Jack.cpp
@@ -5,9 +5,13 @@
 
 #include "../HybridReverb2Processor.h"
 #include "../gui/Editor.h"
-#include <memory>
 #include <jack/jack.h>
 #include <jack/midiport.h>
+#include <memory>
+#include <thread>
+#include <system_error>
+#include <signal.h>
+#include <unistd.h>
 extern AudioProcessor* JUCE_CALLTYPE createPluginFilter();
 
 enum {
@@ -33,6 +37,8 @@ class Application_Jack : public JUCEApplication
 public:
     void initialise(const String &args) override;
     void shutdown() override;
+    void open(const String &client_name);
+    void on_idle();
 
     const String getApplicationName() override
         { return JucePlugin_Name; }
@@ -44,6 +50,7 @@ private:
 
     std::unique_ptr<HybridReverb2Processor> processor_;
     std::unique_ptr<Application_Window> window_;
+    bool is_open_ = false;
 
     struct jack_client_deleter {
         void operator()(jack_client_t *c) const { jack_client_close(c); }
@@ -51,11 +58,56 @@ private:
     std::unique_ptr<jack_client_t, jack_client_deleter> client_;
     jack_port_t *inport_[JackNumInputs] = {};
     jack_port_t *outport_[JackNumOutputs] = {};
+
+    class Idle_Timer : public Timer
+    {
+    public:
+        explicit Idle_Timer(Application_Jack *app) : app_(app) {}
+        void timerCallback() override { app_->on_idle(); }
+    private:
+        Application_Jack *app_ = nullptr;
+    };
+
+    std::unique_ptr<Idle_Timer> idle_timer_;
+
+    static void handle_interrupts();
+    static sig_atomic_t interrupted_by_signal ;
 };
 
 void Application_Jack::initialise(const String &args)
 {
-    jack_client_t *client = jack_client_open(JucePlugin_Name, JackNoStartServer, nullptr);
+    handle_interrupts();
+    open(JucePlugin_Name);
+
+    Idle_Timer *idle_timer = new Idle_Timer(this);
+    idle_timer_.reset(idle_timer);
+    idle_timer->startTimer(50);
+}
+
+void Application_Jack::shutdown()
+{
+    idle_timer_.reset();
+    client_.reset();
+    window_.reset();
+    processor_.reset();
+}
+
+void Application_Jack::on_idle()
+{
+    if (interrupted_by_signal) {
+        setApplicationReturnValue(1);
+        quit();
+        return;
+    }
+}
+
+void Application_Jack::open(const String &client_name)
+{
+    if (is_open_)
+        return;
+    is_open_ = true;
+
+    jack_client_t *client = jack_client_open(client_name.toRawUTF8(), JackNoStartServer, nullptr);
     if (!client)
         throw std::runtime_error("error creating Jack client");
     client_.reset(client);
@@ -102,13 +154,6 @@ void Application_Jack::initialise(const String &args)
     window->setVisible(true);
 }
 
-void Application_Jack::shutdown()
-{
-    client_.reset();
-    window_.reset();
-    processor_.reset();
-}
-
 int Application_Jack::process(jack_nframes_t nframes, void *user_data)
 {
     Application_Jack *self = reinterpret_cast<Application_Jack *>(user_data);
@@ -136,6 +181,36 @@ int Application_Jack::process(jack_nframes_t nframes, void *user_data)
 
     return 0;
 }
+
+void Application_Jack::handle_interrupts()
+{
+    sigset_t sigs;
+    int nsignal = 0;
+
+    sigemptyset(&sigs);
+    for (int signo : {SIGINT, SIGTERM}) {
+        sigaddset(&sigs, signo);
+        nsignal = signo + 1;
+    }
+
+    for (int sig = 1; sig < nsignal; ++sig) {
+        if (!sigismember(&sigs, sig))
+            continue;
+        struct sigaction sa = {};
+        sa.sa_handler = +[](int) { interrupted_by_signal = 1; };
+        sa.sa_mask = sigs;
+        if (sigaction(sig, &sa, nullptr) == -1)
+            throw std::system_error(errno, std::generic_category(), "sigaction");
+    }
+
+    std::thread handler_thread(+[]() { for (;;) pause(); });
+    handler_thread.detach();
+
+    if (pthread_sigmask(SIG_BLOCK, &sigs, nullptr) == -1)
+        throw std::system_error(errno, std::generic_category(), "pthread_sigmask");
+}
+
+sig_atomic_t Application_Jack::interrupted_by_signal = 0;
 
 JUCE_CREATE_APPLICATION_DEFINE(Application_Jack);
 JUCE_MAIN_FUNCTION_DEFINITION;
